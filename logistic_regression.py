4ea35f8fa804ae18730ecd0fd3eb09becd4ea246
nweinr <nweinr17@student.aau.dk>
2021-11-29 08:39:06 +0100

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -19,7 +19,7 @@ data = bs.Data('nyc', 2019, 9)
 station_df = ipu.make_station_df(data, holidays=False)
 traffic_matrices = data.pickle_daily_traffic(holidays=False)
 
-station_df, clusters, labels = ipu.get_clusters(traffic_matrices, station_df, 'business_days', 0, 'k_means', 6, random_state=42)
+station_df, clusters, labels = ipu.get_clusters(traffic_matrices, station_df, 'business_days', 100, 'k_means', 5, random_state=42)
 #%%
 
 ohe_zone = pd.get_dummies(station_df['zone_type'])
@@ -48,3 +48,37 @@ print(f"accuracy is {accuracy*100:.2f}%")
 #     print(zone_type)
 #     print(probs[i])
 #     print(probs[i].argmax())
+
+#%% Statsmodels
+
+import statsmodels.api as sm
+from statsmodels.discrete.discrete_model import MNLogit
+
+data = sm.datasets.scotland.load_pandas()
+
+ohe_zone = pd.get_dummies(station_df['zone_type'])
+X = pd.concat([ohe_zone, station_df[['nearest_subway_dist', 'pop_density', 'n_trips']]], axis=1)
+# X['n_trips'] = X['n_trips']/X['n_trips'].sum()
+# X['nearest_subway_dist'] = X['nearest_subway_dist']/X['nearest_subway_dist'].sum()
+
+# X = ohe_zone
+
+# X = sm.add_constant(X)
+y = station_df['label']
+
+
+LR_model = MNLogit(y, X)
+LR_results = LR_model.fit(maxiter=1000)
+print(LR_results.summary())
+
+# sk_model = LogisticRegression(max_iter=10000)
+# sk_results = sk_model.fit(X,y)
+
+
+
+
+
+
+
+
+
