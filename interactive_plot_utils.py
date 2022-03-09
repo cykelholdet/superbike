6c2ccaec1070c773f59cf1713866705ca749b775
Daniel van Diepen <danie--@hotmail.com>
2022-03-09 15:46:58 +0100

    update nearest subway and railway to use haversine great circle distance

--- interactive_plot_utils.py
+++ interactive_plot_utils.py
@@ -966,12 +966,26 @@ def make_station_df(data, holidays=True, return_land_use=False,
         railways = subways_df[
             ~((subways_df['station']=='subway') | (subways_df['subway']=='yes'))]
         
-        df['nearest_subway'] = df.apply(lambda stat: shapely.ops.nearest_points(stat['coords'], subways.geometry.unary_union)[1], axis=1)
-        df['nearest_subway_dist'] = df.apply(lambda stat: great_circle(stat['coords'].coords[0][::-1], stat['nearest_subway'].coords[0][::-1]).meters, axis=1)
+        # df['nearest_subway'] = df.apply(lambda stat: shapely.ops.nearest_points(stat['coords'], subways.geometry.unary_union)[1], axis=1)
+        # df['nearest_subway_dist'] = df.apply(lambda stat: great_circle(stat['coords'].coords[0][::-1], stat['nearest_subway'].coords[0][::-1]).meters, axis=1)
         
-        df['nearest_railway'] = df.apply(lambda stat: shapely.ops.nearest_points(stat['coords'], railways.geometry.unary_union)[1], axis=1)
-        df['nearest_railway_dist'] = df.apply(lambda stat: great_circle(stat['coords'].coords[0][::-1], stat['nearest_railway'].coords[0][::-1]).meters, axis=1)
+        nearest_subway = nearest_neighbor(df, subways[['geometry']])
+        df['nearest_subway'] = nearest_subway['geometry']
+        df['nearest_subway_dist'] = nearest_subway['distance']
         
+        nearest_railway = nearest_neighbor(df, railways[['geometry']])
+        df['nearest_railway'] = nearest_railway['geometry']
+        df['nearest_railway_dist'] = nearest_railway['distance']
+        
+        nearest_transit = nearest_neighbor(df, subways_df[['geometry']])
+        df['nearest_transit'] = nearest_transit['geometry']
+        df['nearest_transit_dist'] = nearest_transit['distance']
+        
+        # df['nearest_railway'] = df.apply(lambda stat: shapely.ops.nearest_points(stat['coords'], railways.geometry.unary_union)[1], axis=1)
+        # df['nearest_railway_dist'] = df.apply(lambda stat: great_circle(stat['coords'].coords[0][::-1], stat['nearest_railway'].coords[0][::-1]).meters, axis=1)
+        
+        # df['nearest_transit'] = df.apply(lambda stat: shapely.ops.nearest_points(stat['coords'], subways_df.geometry.unary_union)[1], axis=1)
+        # df['nearest_transit_dist'] = df.apply(lambda stat: great_circle(stat['coords'].coords[0][::-1], stat['nearest_transit'].coords[0][::-1]).meters, axis=1)
         
     # Fix some issues with Shapely polygons.
     census_df.geometry = census_df.geometry.buffer(0)
@@ -1629,6 +1643,68 @@ def geodesic_point_buffer(lat, lon, m):
         proj_wgs84)
     buf = Point(0, 0).buffer(m)  # distance in metres
     return shapely.ops.transform(project, buf)
+
+
+def get_nearest(src_points, candidates, k_neighbors=1):
+    """Find nearest neighbors for all source points from a set of candidate points"""
+
+    # Create tree from the candidate points
+    tree = BallTree(candidates, leaf_size=15, metric='haversine')
+
+    # Find closest points and distances
+    distances, indices = tree.query(src_points, k=k_neighbors)
+
+    # Transpose to get distances and indices into arrays
+    distances = distances.transpose()
+    indices = indices.transpose()
+
+    # Get closest indices and distances (i.e. array at index 0)
+    # note: for the second closest points, you would take index 1, etc.
+    closest = indices[0]
+    closest_dist = distances[0]
+
+    # Return indices and distances
+    return (closest, closest_dist)
+
+
+def nearest_neighbor(left_gdf, right_gdf, return_dist=True):
+    """
+    For each point in left_gdf, find closest point in right GeoDataFrame and return them.
+    
+    NOTICE: Assumes that the input Points are in WGS84 projection (lat/lon).
+    """
+    
+    left_geom_col = left_gdf.geometry.name
+    right_geom_col = right_gdf.geometry.name
+    
+    # Ensure that index in right gdf is formed of sequential numbers
+    right = right_gdf.copy().reset_index(drop=True)
+    
+    # Parse coordinates from points and insert them into a numpy array as RADIANS
+    # Notice: should be in Lat/Lon format 
+    left_radians = np.array(left_gdf[left_geom_col].apply(lambda geom: (geom.y * np.pi / 180, geom.x * np.pi / 180)).to_list())
+    right_radians = np.array(right[right_geom_col].apply(lambda geom: (geom.y * np.pi / 180, geom.x * np.pi / 180)).to_list())
+    
+    # Find the nearest points
+    # -----------------------
+    # closest ==> index in right_gdf that corresponds to the closest point
+    # dist ==> distance between the nearest neighbors (in meters)
+    
+    closest, dist = get_nearest(src_points=left_radians, candidates=right_radians)
+
+    # Return points from right GeoDataFrame that are closest to points in left GeoDataFrame
+    closest_points = right.loc[closest]
+    
+    # Ensure that the index corresponds the one in left_gdf
+    closest_points = closest_points.reset_index(drop=True)
+    
+    # Add distance if requested 
+    if return_dist:
+        # Convert to meters from radians
+        earth_radius = 6371000  # meters
+        closest_points['distance'] = dist * earth_radius
+        
+    return closest_points
 
 
 def big_station_df(cities, year=2019, month=None, service_radius=500,
